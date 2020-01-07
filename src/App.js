a3feae15efdfdb781b8fc61c900ae40a72095b02
Shahzad Choudhary <choudharyshahzada@johndeere.com>
2020-01-07 06:38:46 -0600

    nursingHome

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import NursingHome from './components/nursingHomeData';
+
+function App() {
+  return (
+    <div className="App">
+      <NursingHome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nursingHomeData.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react"
+//import List from './list'
+
+const NursingHome = () => {
+const [nursingHome, setNursingHome] = useState([]);
+
+useEffect (() => {
+    fetch ('https://data.medicare.gov/resource/4pq5-n9py.json')
+
+        .then(res => res.json())
+        .then(res => {
+            setNursingHome(res)
+        })
+    }, [])
+
+    return (
+        <div>
+        <h1>This is an API call page for Nursing Homes!</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip Code</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {nursingHome.map((p, index) => {
+                        return <Tr key={index} provider= {p} />
+                    })}
+                </tbody>
+
+            </table>
+        </div>
+    );
+}
+
+const Tr = ({provider}) => {
+    return (
+        <tr>
+            <td>{provider.provider_name}</td>
+            <td>{provider.provider_address}</td>
+            <td>{provider.provider_city}</td>
+            <td>{provider.provider_state}</td>
+            <td>{provider.provider_zip_code}</td>
+        </tr>
+    );    
+}
+export default NursingHome;
