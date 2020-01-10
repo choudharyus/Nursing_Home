47156fb201058721399f3f832327adfd31bc4802
Shahzad Choudhary <choudharyshahzada@johndeere.com>
2020-01-10 12:46:13 -0600

    nursingHome

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ function About () {
     return (
         <div>
             <h1 className="heading1">About</h1>
-            <div className="para1">It has been challenged for the families to find the right nursing home for their loved ones. My plan to design the site that provide the families quick access to the basic information about the nursing home such as; Number of beds (capacity) of the nursning home, Quality star rating of the nursing home, Number of nursing home available in the area, Total number of penalities, violations or fines of the nursing home.
-            <p>Shahzad Choudhary is the developer of this site; he has developed it under the supervison of GA (General Assembly) instructors. A special thanks to the personnel inlcuding GA instructor Ali Spittel, Carlos Godoy, Kenneth Cruz, Nathaniel Stack and Joe Keohan (Squad Supervisior - Project2).</p>
-            <p>I also would like to acknowledge the contribution and support of the electronic resouces such W3-Schools, stackoverflow, github and others to build this site. Thank you everyone for your help and support. </p>
+            <div className="para1">It has been challenged for the families to find the right nursing home for their loved ones. My plan to design the site that provide the families quick access to the basic information about the nursing home such as; Number of beds (capacity) of the nursing home, Quality star rating of the nursing home, Number of nursing home available in the area, Total number of penalties, violations or fines of the nursing home.
+            <p>Shahzad Choudhary is the developer of this site; he has developed it under the supervision of GA (General Assembly) instructors. A special thanks to the personnel including GA instructor Ali Spittel, Carlos Godoy, Kenneth Cruz, Nathaniel Stack and Joe Keohan (Squad Supervisor - Project2).</p>
+            <p>I would like to acknowledge the contribution and support of the electronic resources such W3-Schools, stack-overflow, GitHub and others to build this site. Thank you everyone for your help and support. </p>
             <p>Sincerely, Shahzad Choudhary </p>
             </div>
         </div>
--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -8,7 +8,7 @@ function Disclaimer () {
         <div>
             <h1 className="heading1">Disclaimer</h1>
             <div className="para1">
-                I here to notify that, the following URL (https://data.medicare.gov/resource/4pq5-n9py.json) retrieved from USDA/FDA web resource is purley used for a class project under the supervison of GA (General Assembly) instructors. Although, the data is available to the public but I here clarify that the data retrieved from the above mentioned URL link has not been used for commercial or business purposes, or anyother malicious intention.
+                I here, do notify that, the following URL (https://data.medicare.gov/resource/4pq5-n9py.json) retrieved from USDA/FDA web resource is purely used for a class project under the supervision of GA (General Assembly) instructors. Although, the data is available to the public, but I here clarify that the data retrieved from the above-mentioned URL link has not been used for commercial or business purposes, or another malicious intention.
                 <p>Sincerely, Shahzad Choudhary (Developer)</p>
             </div>    
         </div>
