7338bb55afb364e83a2cd30bfe84e5a428a0cd78
Shahzad Choudhary <choudharyshahzada@johndeere.com>
2020-01-10 12:49:16 -0600

    nursingHome

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About () {
     return (
         <div>
             <h1 className="heading1">About</h1>
-            <div className="para1">It has been challenged for the families to find the right nursing home for their loved ones. My plan to design the site that provide the families quick access to the basic information about the nursing home such as; Number of beds (capacity) of the nursing home, Quality star rating of the nursing home, Number of nursing home available in the area, Total number of penalties, violations or fines of the nursing home.
+            <div className="para1">It can be challenging for families to find the right nursing home for their loved one. My plan to design the site that provide the families quick access to the basic information about the nursing home such as; Number of beds (capacity) of the nursing home, Quality star rating of the nursing home, Number of nursing home available in the area, Total number of penalties, violations or fines of the nursing home.
             <p>Shahzad Choudhary is the developer of this site; he has developed it under the supervision of GA (General Assembly) instructors. A special thanks to the personnel including GA instructor Ali Spittel, Carlos Godoy, Kenneth Cruz, Nathaniel Stack and Joe Keohan (Squad Supervisor - Project2).</p>
             <p>I would like to acknowledge the contribution and support of the electronic resources such W3-Schools, stack-overflow, GitHub and others to build this site. Thank you everyone for your help and support. </p>
             <p>Sincerely, Shahzad Choudhary </p>
